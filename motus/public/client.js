9fa8f5e3773da3475612178e9ef2e0fd519e5a3d
LasgleizesDavid <118431860+LasgleizesDavid@users.noreply.github.com>
2024-03-15 22:28:01 +0100

    modifie client.js pour fonctionnement global

--- motus/public/client.js
+++ motus/public/client.js
@@ -1,25 +1,40 @@
 // public/client.js
-var nbTry = 0;
 
 $(document).ready(function(){
+    let isCorrect = false;
+    let username = '';
+    let scoring = null;
+
+    $.get('/wordLength', function(result){
+        $('#wordLength').html(result.tmp1);
+        $('#tryNumber').html(result.tmp2);
+    });
+    
     $('#guessForm').on('submit', function(event){
-
-        nbTry++;
-
-        event.preventDefault(); // Prevent default form submission
-
-        // Get user guess
+        // Récupérer la devinette de l'utilisateur
         var guess = $('#guess').val();
 
-        url = '/guess/' + guess;
-        console.log(url);
+        // Récupérer le résultat depuis le serveur en fonction de la devinette de l'utilisateur
+        $.get('/guess/' + guess, function(result){
+            // Afficher le résultat
+            $('#result').html(result.result);
+            $('#tryNumber').html(result.tryNumber1  );
+            username = result.username;
 
-        // Fetch word for the day from server
-        $.get(url, function(result){
-            // Display the result
-            $('#result').html(result);
-
+            if (result.tryNumber1 > 6 || result.isCorrect) {
+                $('#guessForm button[type="submit"]').prop('disabled', true);
+                scoring = result.scoring;
+                $('#specialButton').show();
+            }
         });
+
+        // Empêcher le formulaire de se soumettre normalement
+        event.preventDefault();
+    });
+
+    $('#specialButton').on('click', function() {
+        // Appeler la fonction displayScores() avec le score récupéré
+        displayScores(scoring);
     });
 });
 
